72129127277dd3bf8c9d0006ab34d0f9552a54b6
Tejpartap Singh <54923383+tejsidhu@users.noreply.github.com>
2021-03-23 15:37:17 +1100

    Add files via upload

---
+++ console/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using service;
+
+namespace console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var services = new ServiceCollection();
+            services.AddTransient<GetContacts>();
+
+            var provider = services.BuildServiceProvider();
+
+            using (var service = provider.GetService<GetContacts>())
+            {
+                var result = service.GetData();
+            }
+
+            Console.ReadKey();
+        }
+
+
+    }
+}
